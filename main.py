fa0d3c4fee6f4d0932ce8c287a3d8b6f82a3727e
Kaleb-Aguirre03 <820930@mystma.org>
2021-10-14 09:19:53 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+#define a function that 
+def shareFair(x, y):
+    if x % y == 0:
+        return True
+
+    else:
+        return False
+
+#test the function
+print(shareFair(10, 3))
